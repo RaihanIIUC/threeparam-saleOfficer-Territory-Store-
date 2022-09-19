7ad5cf794a802634ed33ed7527dd2dcb810948e0
RaihanIIUC <mdraihan4835@gmail.com>
2022-09-19 17:15:48 +0600

    add with unfixed components

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::post('sms',[BdappsController::class,'sms']);
 
 Route::get('test', function (){
