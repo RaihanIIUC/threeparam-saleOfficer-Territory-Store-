e1313e8b3b24f027e2c9ada5b28228c8eca9afb0
RaihanIIUC <mdraihan4835@gmail.com>
2022-09-19 17:27:38 +0600

    finally add loggin && middlware works fine in page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,39 @@
+<x-guest-layout>
+    <x-auth-card>
+
+
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('login') }}" class="pt-8">
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" :value="__('Email')" />
+
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
+            </div>
+
+
+
+            <div class="flex items-center justify-center mt-4">
+                <x-primary-button class="text-center bg-emerald-600	">
+                    {{ __('Log in') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
